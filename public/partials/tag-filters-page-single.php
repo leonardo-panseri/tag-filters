d923dad92c3a20a6fe441a07af785baaf2bc9ae4
Leonardo Panseri <leonardo.panseri@gmail.com>
2022-02-22 12:32:55 +0100

    refactor template file

--- public/partials/tag-filters-page-single.php
+++ public/partials/tag-filters-page-single.php
@@ -10,54 +10,29 @@
  */
 ?>
 
-<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+<?php get_header() ?>
 
-<!-- wp:group {"tagName":"main"} -->
-<main class="wp-block-group"><!-- wp:group {"layout":{"inherit":true}} -->
-    <div class="wp-block-group"><!-- wp:post-title {"level":1,"align":"wide","style":{"spacing":{"margin":{"bottom":"var(--wp--custom--spacing--medium, 6rem)"}}}} /-->
+<main class="wp-block-group">
+    <div class="wp-block-group">
+        <?php the_title() ?>
 
-        <!-- wp:post-featured-image {"align":"wide","style":{"spacing":{"margin":{"bottom":"var(--wp--custom--spacing--medium, 6rem)"}}}} /-->
+        <?php the_post_thumbnail('full'); ?>
 
-        <!-- wp:separator {"align":"wide","className":"is-style-wide"} -->
         <hr class="wp-block-separator alignwide is-style-wide"/>
-        <!-- /wp:separator --></div>
-    <!-- /wp:group -->
+    </div>
 
-    <!-- wp:spacer {"height":32} -->
     <div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
-    <!-- /wp:spacer -->
 
-    <!-- wp:post-content {"layout":{"inherit":true}} /-->
-
-    <!-- wp:spacer {"height":32} -->
-    <div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
-    <!-- /wp:spacer -->
-
-    <!-- wp:group {"layout":{"inherit":true}} -->
-    <div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex"}} -->
-        <div class="wp-block-group">
-            <?php
-            echo <<<END
-                <div>
-                    <p>Test paragraph</p>
-                </div>
-                END;
-
-            ?>
+    <?php
+    echo <<<END
+        <div>
+            <p>Test p</p>
         </div>
-        <!-- /wp:group -->
+        END;
 
-        <!-- wp:spacer {"height":32} -->
-        <div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
-        <!-- /wp:spacer -->
+    ?>
 
-        <!-- wp:separator {"className":"is-style-wide"} -->
-        <hr class="wp-block-separator is-style-wide"/>
-        <!-- /wp:separator -->
+    <div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
+</main>
 
-        <!-- wp:post-comments /--></div>
-    <!-- /wp:group --></main>
-<!-- /wp:group -->
-
-<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
-
+<?php get_footer() ?>
